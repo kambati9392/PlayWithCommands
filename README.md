33145763bd389729498518375e903cef21ebfa24
Rammohan <reddy.kambati@gmail.com>
2024-09-11 22:13:38 +0530

    simply

---
+++ README.md
@@ -0,0 +1,1 @@
+# Commandds 
---
+++ To_Play_Commands.java
@@ -0,0 +1,7 @@
+package com.bridzlab.commands;
+
+public class To_Play_Commands {
+     public static void main(String[] args) {
+		System.out.println("My name is RAM reddy");
+	}
+}
